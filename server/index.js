188024fe635ad178f5e5353412829efdbb00434a
mitchell stone <themitch.stone@gmail.com>
2020-08-24 15:34:13 -0600

    finished 4

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const products = require('../products.json')
+const getProducts = require('./getProducts')
 
 
 const app = express()
 const SERVER_PORT = 4296
+
+
+app.get(`/api/products`, (req, res) => {
+    res.status(200).send(products)
+})
 
 app.listen(SERVER_PORT, () =>console.log(`listening on port ${SERVER_PORT}`))
 
